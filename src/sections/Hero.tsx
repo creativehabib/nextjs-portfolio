f97bd60db0e3fb07f7e44b1e27c02729d8087b15
creativehabib <shamolrahaman@gmail.com>
2024-09-23 16:19:40 +0600

    some update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,30 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image src={ memojiImage } className="size-[100px" alt={'Person peeking from behind the laptop'}/>
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">Available for new project</div>
+          </div>
+        </div>
+        <h1 className="font-serif text-3xl text-center mt-4">Building Exceptional User Experiences</h1>
+        <p>I specialize in transforming designs into functional, high-performing web applications. Lets discuss your next project</p>
+        <div>
+          <button>
+            <span>Explore My Work</span>
+            <ArrowDown/>
+          </button>
+          <button>
+            <span></span>
+            <span>Lets Connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 };
